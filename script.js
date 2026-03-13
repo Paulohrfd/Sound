144950557dc8d59b20abb7bae8402a3c66e19785
Paulohrfd <bprvozaoo@gmail.com>
2026-03-13 03:54:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -878,9 +878,7 @@ function restartGame() {
 
   
 
-  navigator.clipboard.writeText(`${text}\n${window.location.href}`)
-    .then(() => alert("Resultado copiado para compartilhar!"))
-    .catch(() => alert(text));
+ 
 
 }
 
@@ -1085,6 +1083,7 @@ function shareChampion() {
     .then(() => alert("Resultado copiado para compartilhar!"))
     .catch(() => alert(text));
 }
+
 
 
 
